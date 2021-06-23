fac3acbcdfad042650bf63a95335d14232b3cb59
Theo Vora <theo.vora@gmail.com>
2021-06-23 17:40:27 -0600

    Initial Commit

---
+++ Gradients-SwiftUI/Gradients_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gradients_SwiftUIApp.swift
+//  Gradients-SwiftUI
+//
+//  Created by Theo Vora on 6/23/21.
+//
+
+import SwiftUI
+
+@main
+struct Gradients_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
