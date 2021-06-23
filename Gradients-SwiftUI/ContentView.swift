09e56c67a228369642abdf2a5b744c807d4ecc82
Theo Vora <theo.vora@gmail.com>
2021-06-23 17:57:46 -0600

    Show Linear, Angular, and Radial Gradients

--- Gradients-SwiftUI/ContentView.swift
+++ Gradients-SwiftUI/ContentView.swift
@@ -8,9 +8,20 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let gradient = Gradient(colors: [.red, .green, .blue, .purple])
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Linear").font(.title)
+            LinearGradient(gradient: gradient, startPoint: .leading, endPoint: .trailing)
+            Text("Angular").font(.title)
+            AngularGradient(gradient: gradient, center: .center)
+            Text("Radial").font(.title)
+            RadialGradient(gradient: gradient, center: .center, startRadius: 1, endRadius: 150)
+            Text("")
+        }
+        
     }
 }
 
